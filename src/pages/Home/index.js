f20be3dc0ea6ced9fc100cbc0ccf1ac34bfac1a5
briangregoryholmes <briangregoryholmes@gmail.com>
2023-02-18 11:32:02 -0500

    Added DATABASE_URL

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { PathWidget, HorizontalScroll } from '../../components';
+import { NavBar, PathWidget, HorizontalScroll } from '../../components';
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <NavBar />
       <Link to="/discover">Discover</Link>
       <HorizontalScroll>
         <PathWidget />
